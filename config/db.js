b3417eb7de9768e7a4ed040864494584685e687b
amitpydev32 <amitsharma36547@gmail.com>
2025-09-19 12:37:29 +0530

    updated env file

--- config/db.js
+++ config/db.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+  await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    console.log("MongoDB connected successfully");
   } catch (err) {
-    throw err;
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // exit if DB connection fails
   }
 };
+
 
 module.exports = connectDB;
